9f47cfb4cb3be7299301110bf5f80ab4ea66f898
Rejaurrahman5 <mdrejaurrahmanr@gmail.com>
2026-02-20 00:22:21 +0600

    hi raju

---
+++ script/cashout.js
@@ -0,0 +1,14 @@
+document.getElementById("cashout-btn").addEventListener("click", function() {
+    // 1. get the agent number & validate
+   const cashoutNumberInput = document.getElementById("cashout-number");
+   const cashoutNumber =cashoutNumberInput.value;
+   console.log(cashoutNumber);
+
+    // 2. get the amount, validate, convert to number
+    // 3. get the current balance, validate, convert to number
+    // 4. calculate the new balance
+    // 5.get the pin and verify
+    // 5.1true:: show an alart > set balance
+    // 5.2 true:: show an error alart > return
+
+})
